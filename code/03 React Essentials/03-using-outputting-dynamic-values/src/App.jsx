2a965da621e2aefa78bac4ae66038ec699952a0f
Alexeigithubproject <alexeiprsk@gmail.com>
2025-05-12 07:59:18 +0300

    oo

--- code/03 React Essentials/03-using-outputting-dynamic-values/src/App.jsx
+++ code/03 React Essentials/03-using-outputting-dynamic-values/src/App.jsx
@@ -1,3 +1,9 @@
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+import configImg from './assets/config.png';
+import jsxUi from './assets/jsx-ui.png';
+import stateMgmt from './assets/state-mgmt.png'
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -9,22 +15,57 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src= { reactImg } alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {description} React concepts you will need for almost any app you are
+        { description } React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={ props.image } alt={ props.title }/>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+            title="Components" 
+            description="The core UI building block."
+            image={ componentsImg }/>
+
+            <CoreConcept 
+            title="Configuration" 
+            description="The core UI building block."
+            image={ configImg }/>
+            
+            <CoreConcept 
+            title="JSX-UI" 
+            description="The core UI building block."
+            image={ jsxUi } />
+            <CoreConcept 
+            title="State Management" 
+            description="The core UI building block."
+            image={ stateMgmt }/>
+          </ul>
+
+        </section>
+
       </main>
     </div>
   );
