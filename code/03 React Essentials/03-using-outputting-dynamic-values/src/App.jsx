1c467dd46619fe1c47899d69f3f4a8d1feeedd22
alexei <alexeiprsk@gmail.com>
2025-05-12 18:35:28 +0300

    moving on

--- code/03 React Essentials/03-using-outputting-dynamic-values/src/App.jsx
+++ code/03 React Essentials/03-using-outputting-dynamic-values/src/App.jsx
@@ -1,39 +1,7 @@
-import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
-import configImg from './assets/config.png';
-import jsxUi from './assets/jsx-ui.png';
-import stateMgmt from './assets/state-mgmt.png'
+import {CORE_CONCEPTS} from './data.js';
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src= { reactImg } alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        { description } React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={ props.image } alt={ props.title }/>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
 
 
 function App() {
@@ -44,24 +12,19 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept 
-            title="Components" 
-            description="The core UI building block."
-            image={ componentsImg }/>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
 
-            <CoreConcept 
-            title="Configuration" 
-            description="The core UI building block."
-            image={ configImg }/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
             
             <CoreConcept 
-            title="JSX-UI" 
-            description="The core UI building block."
-            image={ jsxUi } />
+            title={CORE_CONCEPTS[2].title}
+            description={CORE_CONCEPTS[2].description}
+            image={CORE_CONCEPTS[2].image}/>
+
             <CoreConcept 
-            title="State Management" 
-            description="The core UI building block."
-            image={ stateMgmt }/>
+            title={CORE_CONCEPTS[3].title}
+            description={CORE_CONCEPTS[3].description}
+            image={CORE_CONCEPTS[3].image}/>
           </ul>
 
         </section>
