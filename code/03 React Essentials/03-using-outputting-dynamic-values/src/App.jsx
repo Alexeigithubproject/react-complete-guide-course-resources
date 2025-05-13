b0ed87c8f289d8b5a5650fa0ed44ff86bfdebf57
alexei <alexeiprsk@gmail.com>
2025-05-13 20:45:41 +0300

    useState

--- code/03 React Essentials/03-using-outputting-dynamic-values/src/App.jsx
+++ code/03 React Essentials/03-using-outputting-dynamic-values/src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+
 import {CORE_CONCEPTS} from './data.js';
 import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
-
+import TabButton from './components/TabButton.jsx';
 
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState("click");  /*always returns 2 elements */
+
+
+  function HandleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  };
+
   return (
     <div>
       <Header />
@@ -27,6 +37,16 @@ function App() {
             image={CORE_CONCEPTS[3].image}/>
           </ul>
 
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => HandleSelect("components")} >Components</TabButton>
+            <TabButton onSelect={() => HandleSelect("jsx")} >JSX</TabButton>
+            <TabButton onSelect={() => HandleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => HandleSelect("state")}>State</TabButton>
+          </menu>
+          {selectedTopic}
         </section>
 
       </main>
