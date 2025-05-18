404201cd5222d745ee81ad70f73f75b589ebebb6
Alexeigithubproject <alexeiprsk@gmail.com>
2025-05-18 10:39:03 +0300

    editable name

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,22 +1,27 @@
 import { useState } from "react";
 
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [ isEditing, setisEditing ] = useState(false);
 
     function HandleEditClick() {
-        setisEditing(true)
-
+      setisEditing(prev => !prev)
     }
 
+    function HandleChange(event) {
+      console.log(event.target.value)
+      setPlayerName(event.target.value)
+
+    }
     
     return (
         <li>
           <span className="player">
-            {!isEditing ? <span className="player-name">{name}</span> : <input className="player-name"></input>}
+            {!isEditing ? <span className="player-name">{playerName}</span> : <input className="player-name" onChange={HandleChange} value={playerName} required />}
             <span className="player-symbol">{symbol}</span>
           </span>
-          <button onClick={HandleEditClick}>Edit</button>
+          <button onClick={HandleEditClick}>{!isEditing ? "Edit" : "Save"}</button>
         </li>
     );
 }
